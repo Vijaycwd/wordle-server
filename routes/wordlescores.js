3dc3ab8e128130cd2d3dc3a499d3899698f33bb7
Vijaycwd <vijisubramani25@gmail.com>
2024-09-25 17:05:37 +0530

    Update wordlescores.js

--- routes/wordlescores.js
+++ routes/wordlescores.js
@@ -65,13 +65,12 @@ router.route('/wordle-score').post(async (req, res) => {
 // Get total games played and statistics
 router.get('/:useremail', async (req, res) => {
     try {
-        const stats = await wordleSchema.findOne({ useremail: req.params.useremail });
+        const stats = await wordleScoreObject.findOne({ useremail: req.params.useremail });
         if (!stats) return res.status(404).json({ message: 'User not found' });
         res.status(200).json(stats);
     } catch (err) {
         res.status(500).json({ message: err.message });
     }
 });
-
 
 module.exports = router;
