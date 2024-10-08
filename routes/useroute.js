17521f9ded4f44e44a0a9e6bda41eea3a5ee4e8a
Vijaycwd <vijisubramani25@gmail.com>
2024-10-08 15:24:21 +0530

    Update useroute.js

--- routes/useroute.js
+++ routes/useroute.js
@@ -24,7 +24,7 @@ const path = require('path');
 
 console.log(path.join(__dirname));
 // Ensure uploads directory exists
-const uploadPath = path.join(__dirname, '.../public/uploads/');
+const uploadPath = path.join(__dirname, './../public/uploads/');
 
 console.log(uploadPath);
 if (!fs.existsSync(uploadPath)) {
